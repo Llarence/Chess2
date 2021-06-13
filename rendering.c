f2c744146dd8ea1a7cd2033d2b78ab8f9c8663a8
A Terrible Name <you@example.com>
2021-06-13 15:11:03 -0700

    double buffering

--- rendering.c
+++ rendering.c
@@ -15,7 +15,7 @@
 #include "fen.c"
 #include "ai.c"
 
-#define AI_WHITE FALSE
+#define AI_WHITE TRUE
 #define AI_BLACK TRUE
 
 Game game;
@@ -216,7 +216,7 @@ void render(){
         drawPiece(game.board[selectedX][selectedY], mouseX - halfSizeX, mouseX + halfSizeX, mouseY - halfSizeY, mouseY + halfSizeY);
     }
 
-    glFlush();
+    glutSwapBuffers();
 }
 
 void mouseMove(int x, int y){
@@ -377,7 +377,7 @@ void initWindow(){
     int argc = 0;
     char *argv[0];
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitDisplayMode(GLUT_DOUBLE);
 
     glutInitWindowSize(800, 800);
     glutInitWindowPosition(100, 100);
